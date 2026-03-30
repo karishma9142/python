9e7271182c65fa989312eb33e2a0ead6a9744538
karishma9142 <karishmacair47050@gmail.com>
2026-03-30 18:03:46 +0530

    learn conditional statment nad while loop

---
+++ operator/logical.py
@@ -0,0 +1,13 @@
+has_high_income = True
+has_good_credit = False
+
+if has_good_credit or has_high_income :
+    print("Eligible for loan")
+else :
+    print("not Eligible for loan") 
+
+
+
+has_criminal_record = False
+if has_high_income and not has_criminal_record :
+    print("Eligible for loan") 
